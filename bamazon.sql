d9d8d7bf3fc3a8552aa8e93aefddad72ad1a5896
Barbara Whiting <whitingba@gmail.com>
2019-09-03 19:09:07 -0600

    mock data added to table

--- bamazon.sql
+++ bamazon.sql
@@ -5,7 +5,7 @@ create database bamazon;
 use bamazon;
 
 create table products (
-id integer (10) auto_increment not null,
+item_id integer (10) not null,
 product_name varchar (50),
 department_name varchar (50),
 price decimal (10, 2) null,
@@ -13,4 +13,9 @@ stock_quantity int null
 );
 
 
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (17, "Permanent Whiteboard Markers", "Home and Household", 9.99, 75);
 
+update products set product_name = "Phose Headphones" where product_name = "Fose Headphones";
+
+SET SQL_SAFE_UPDATES = 0;
